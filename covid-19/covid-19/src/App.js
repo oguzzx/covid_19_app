041c9f26b1caf2938cc7ab1497672f8628a7e74e
oguzzx <1204225052@nku.edu.tr>
2022-09-26 14:27:49 +0300

    covid_19_app
    türkiye covid19 günlük veriler

---
+++ covid-19/covid-19/src/App.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [veri, setVeri] = useState();
+  const [tarih, setTarih] = useState();
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://raw.githubusercontent.com/ozanerturk/covid19-turkey-api/master/dataset/timeline.json"
+      )
+      .then((res) => setVeri(res.data[tarih]))
+      .catch((err) => console.log(err));
+  }, [veri, tarih]);
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 mx-auto mt-4">
+            <h2 className="text-center text-white display-3">
+              TÜRKİYE COVİD-19 ARAMA MOTORU
+            </h2>
+            <input
+              type="text"
+              placeholder="GG/AA/YY"
+              className="form-control"
+              onChange={(e) => setTarih(e.target.value)}
+            />
+            <table className="table table-striped text-white">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Test Sayısı</th>
+                  <th scope="col">Hasta Sayısı</th>
+                  <th scope="col">Vefay Sayısı</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className={veri === undefined ? "bg-danger" : "bg-success"}>
+                  <th className="text-white" scope="row">
+                    {veri === undefined ? "veri bekleniyor" : veri.date}
+                  </th>
+                  <td className="text-white">
+                    {veri === undefined ? "veri bekleniyor" : veri.totalTests}
+                  </td>
+                  <td className="text-white">
+                    {veri === undefined ? "veri bekleniyor" : veri.patients}
+                  </td>
+                  <td className="text-white">
+                    {veri === undefined ? "veri bekleniyor" : veri.deaths}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
